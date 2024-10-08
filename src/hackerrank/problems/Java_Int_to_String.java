99c0a67300866066ef1c10cacbc34fbd0c6e8fdd
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-10-08 10:59:18 +0600

    Int to String

---
+++ src/hackerrank/problems/Java_Int_to_String.java
@@ -0,0 +1,30 @@
+
+package hackerrank.problems;
+import java.util.Scanner;
+/**
+ *
+ * @author Banna
+ */
+public class Java_Int_to_String {
+
+   
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        
+        int n = in.nextInt();
+        
+        try {
+            String s = Integer.toString(n);
+            //String s = String.valueOf(n);
+           
+            System.out.println("Good job");
+        } catch (Exception e) {
+            System.out.println("Wrong answer");
+        }
+    }
+}
+
+    
+}
