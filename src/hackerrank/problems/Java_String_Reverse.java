ebef058817ed42b072926b5e06a62cd70fcfd113
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-10-17 02:17:53 +0600

    sTRING reverse

---
+++ src/hackerrank/problems/Java_String_Reverse.java
@@ -0,0 +1,27 @@
+
+package hackerrank.problems;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Banna
+ */
+public class Java_String_Reverse {
+
+
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        
+        String reversed = new StringBuilder(A).reverse().toString();
+        
+        
+        if (A.equals(reversed)) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+    
+}
