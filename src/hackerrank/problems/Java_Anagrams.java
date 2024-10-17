e89c14b5fcb90ae648cfd2f2653108a6b1278358
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-10-17 23:57:24 +0600

    Anagrams V2.0

--- src/hackerrank/problems/Java_Anagrams.java
+++ src/hackerrank/problems/Java_Anagrams.java
@@ -1,6 +1,9 @@
 
 package hackerrank.problems;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  *
  * @author Banna
@@ -8,15 +11,42 @@ package hackerrank.problems;
 public class Java_Anagrams {
 
     static boolean isAnagram(String a, String b){
-        a= a.toLowerCase();
-        b=b.toLowerCase();
+        char[] c1 = a.toCharArray();
+        char[] c2 = b.toCharArray();
         
-        if(a.length() != b.length()){
-            return false;
+        if(c1.length != c2.length){
+//            System.out.println("Not Anagrams");
+                return false;
         }
+        
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+        
+        for(int i = 0; i<c1.length; i++){
+            if(c1[i] != c2[i]){
+//              System.out.println("Not Anagrams");  
+                return false;
+            }
+        }
+//        System.out.println("Anagrams");
+        return true;
+//        a= a.toLowerCase();
+//        b=b.toLowerCase();
+//        
+//        if(a.length() != b.length()){
+//            return false;
+//        }
+            
+
     }
     public static void main(String[] args) {
-       
+       Scanner sc = new Scanner(System.in);
+       String a = sc.next();
+       String b =sc.next();
+       sc.close();
+      boolean bt= isAnagram(a, b);
+      System.out.println((bt) ? "Anagrams": "Not Anagrams");
+        
     }
     
 }
