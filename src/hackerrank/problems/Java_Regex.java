4d3aab036322ade0bab4f1683b5a5ec3d776bfef
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-10-23 23:57:31 +0600

    java Regex

---
+++ src/hackerrank/problems/Java_Regex.java
@@ -0,0 +1,28 @@
+
+package hackerrank.problems;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Banna
+ */
+public class Java_Regex {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        while (in.hasNext()) {
+            String IP = in.next();
+            System.out.println(IP.matches(new MyRegex().pattern));
+        }
+        in.close();
+    }
+}
+
+class MyRegex {
+    // Regular expression pattern for a valid IP address
+    public String pattern = 
+        "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.)"  // First 3 octets
+        + "{3}"  // Repeat the pattern for 3 octets
+        + "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$";  // Last octet
+}
+
--- src/hackerrank/problems/Java_String_Tokens.java
+++ src/hackerrank/problems/Java_String_Tokens.java
@@ -1,5 +1,5 @@
 
-package hackerrank;
+package hackerrank.problems;
 import java.util.Scanner;
 /**
  *
