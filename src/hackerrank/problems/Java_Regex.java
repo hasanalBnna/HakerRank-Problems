27d4452e6547a2d21a49ffe27f556835943b766f
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-10-24 00:30:40 +0600

    java Regex v.3

--- src/hackerrank/problems/Java_Regex.java
+++ src/hackerrank/problems/Java_Regex.java
@@ -20,16 +20,19 @@ public class Java_Regex {
 
 class MyRegex {
     // Regular expression pattern for a valid IP address
-    public String pattern = 
-        "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.)"  // First 3 octets
-        + "{3}"  // Repeat the pattern for 3 octets
-        + "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$";  // Last octet
+//    public String pattern = 
+//        "^((25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])\\.)"  // First 3 octets
+//        + "{3}"  // Repeat the pattern for 3 octets
+//        + "(25[0-5]|2[0-4][0-9]|1[0-9][0-9]|[1-9]?[0-9])$";  // Last octet
     
 //    public String pattern = 
 //        "^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?|0)\\.)"  // First 3 octets
 //        + "{3}"  // Repeat for 3 octets
 //        + "(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]?|0)$";
     
-    
+    public String pattern = 
+        "^((0?[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])\\.)"  // First 3 octets
+        + "{3}"  // Repeat for 3 octets
+        + "(0?[0-9]{1,2}|1[0-9]{2}|2[0-4][0-9]|25[0-5])$";
 }
 
