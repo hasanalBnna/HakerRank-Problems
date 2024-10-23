e84bfb4adcfe33f677243a91e5f4b1ccd81f9c13
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-10-23 23:46:16 +0600

    Pattern syntex checker( clean the code)

--- src/hackerrank/problems/Pattern_Syntax_Checker.java
+++ src/hackerrank/problems/Pattern_Syntax_Checker.java
@@ -16,18 +16,14 @@ public class Pattern_Syntax_Checker {
         
         // while(testCases>0){
         // 	String pattern = in.nextLine();
-        
-        // Step 1: Loop through each test case
+       
         for (int i = 0; i < testCases; i++) {
             String pattern = scanner.nextLine();
             
             try {
-                // Step 2: Try to compile the pattern
                 Pattern.compile(pattern);
-                // Step 3: If no exception is thrown, the pattern is valid
                 System.out.println("Valid");
             } catch (PatternSyntaxException e) {
-                // Step 4: If an exception is thrown, the pattern is invalid
                 System.out.println("Invalid");
             }
         }
