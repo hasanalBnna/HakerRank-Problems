6f87950f0bbedc0e26b33c3d1d769172fc9b041b
Kazi Hasan Al Banna <kazi.hasanalbanna59@gmail.com>
2024-10-26 22:29:52 +0600

    duplicate remove v.2

--- src/hackerrank/problems/Java_Regex_2_Duplicate_Words.java
+++ src/hackerrank/problems/Java_Regex_2_Duplicate_Words.java
@@ -26,8 +26,12 @@ public class Java_Regex_2_Duplicate_Words {
             Matcher m = p.matcher(input);
             
             // Replace all consecutive duplicates
-            while (m.find()) {
-                input = input.replaceAll(m.group(0), m.group(1));
+//            while (m.find()) {
+//                input = input.replaceAll(m.group(0), m.group(1));
+//            }
+              while (m.find()) {
+                input = m.replaceAll("$1"); 
+                m = p.matcher(input); 
             }
             
             // Print the result after replacement
